90d05376c5a585e0955fe13a9e76e3458acfe762
robbianobastien <bastien.robbiano@live.fr>
2024-08-12 16:05:22 +0200

    page d'erreur

--- src/Page/NotFound.jsx
+++ src/Page/NotFound.jsx
@@ -1,6 +1,12 @@
+import { Link } from 'react-router-dom'
+import './NotFound.scss'
 function NotFound() {
     return(
-        <h1>404</h1>
+        <div className='notfound'>
+        <h1 className='notfound__title'>404</h1>
+        <h2 className='notfound__txt'>Oups! La page que vous demandez n'existe pas.</h2>
+        <Link className='notfound__link' to="/">Retourner sur la page d'accueil</Link>
+        </div>
     ) 
 }
 
